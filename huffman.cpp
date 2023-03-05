9ce00c1bc3dd9e5e5a7b4984bd7ba40e42c33232
Aiden <marcin.madry@interia.com>
2023-03-05 20:57:38 +0100

    Allow huffman program to parse text files

--- huffman.cpp
+++ huffman.cpp
@@ -11,12 +11,13 @@
 */
 #define UNUSED(x) ((void)(x))
 #define MAX_CAP 32
+#define TEMP_BUFFER_SZ 16
 
-typedef uint8_t u8;
 typedef uint32_t u32;
+typedef int32_t s32;
 
 struct Hf_Node {
-    u8 character;
+    char character;
     u32 frequency;
 
     Hf_Node *left;
@@ -38,21 +39,56 @@ Hf_Node *heap_get_min(Min_Heap *heap);
 
 void huffman_print_tree(Hf_Node *root, u32 indent);
 void huffman_print_codes(Hf_Node *root, std::string str);
-    
+
+char *file_read_contents(const char *filename);
+
 int main(int argc, char **argv)
 {
-    UNUSED(argc);
-    UNUSED(argv);
-
     Min_Heap heap = {0};
+    
+    if (argc > 1) {
+        const char *contents = file_read_contents(argv[1]);
+        
+        Hf_Node node = {0};
+        char temp_buffer[TEMP_BUFFER_SZ] = {0};
+        u32 buff_idx = 0;
+        
+        for (u32 i = 0; contents[i] != '\0'; ++i) {
+            const char c = contents[i];
 
-    // NULLPTR <=> 0
-    heap_insert(&heap, { 'a', 15, 0, 0 });
-    heap_insert(&heap, { 'b', 12, 0, 0 });
-    heap_insert(&heap, { 'c', 5, 0, 0 });
-    heap_insert(&heap, { 'd', 4, 0, 0 });
-    heap_insert(&heap, { 'e', 1, 0, 0 });
-    heap_insert(&heap, { 'f', 45, 0, 0 });
+            if (c == '\n') {
+                temp_buffer[buff_idx] = '\0';
+                buff_idx = 0;
+
+                node.frequency = atoi(temp_buffer);
+                heap_insert(&heap, node);
+            } else if (c == ',') {
+                node.character = contents[i - 1];
+            } else if (isdigit(c) && buff_idx < TEMP_BUFFER_SZ - 1) {
+                temp_buffer[buff_idx] = c;
+                buff_idx += 1;
+            }
+        }
+
+        // EOF
+        if (buff_idx != 0) {
+            temp_buffer[buff_idx] = '\0';
+            buff_idx = 0;
+
+            node.frequency = atoi(temp_buffer);
+            heap_insert(&heap, node);
+        }
+        
+        free((void *)contents);
+    } else {
+        // DEFAULT VALUES WHEN FILE NOT PROVIDED
+        heap_insert(&heap, { 'a', 15 });
+        heap_insert(&heap, { 'b', 12 });
+        heap_insert(&heap, { 'c', 5 });
+        heap_insert(&heap, { 'd', 4 });
+        heap_insert(&heap, { 'e', 1 });
+        heap_insert(&heap, { 'f', 45 });
+    }
     
     while (heap.size != 1) {
         Hf_Node *left = heap_get_min(&heap);
@@ -178,6 +214,29 @@ void huffman_print_codes(Hf_Node *root, std::string str)
 {
     if (root->character != '\0') printf("%c - %s\n", root->character, str.c_str());
     
-    if (root->left) huffman_print_codes(root->left, str + "1");
-    if (root->right) huffman_print_codes(root->right, str + "0");
+    if (root->left) huffman_print_codes(root->left, str + "0");
+    if (root->right) huffman_print_codes(root->right, str + "1");
+}
+
+char *file_read_contents(const char *filename)
+{
+    FILE *file = fopen(filename, "rb");
+
+    if (file == 0) {
+        fprintf(stderr, "[ERROR]: Could not open a file %s\n", filename);
+        exit(1);
+    }
+    
+    fseek(file, 0, SEEK_END);
+    s32 size = ftell(file); 
+    fseek(file, 0, SEEK_SET);
+    
+    char *contents = (char *)malloc((sizeof(char) * size) + 1);
+    
+    fread(contents, sizeof(char), size, file);
+    contents[size] = '\0';
+    
+    fclose(file);
+    
+    return(contents);
 }
